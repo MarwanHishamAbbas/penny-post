fb9775c620b8203699289a0633ac7e6ac4422c96
Marwan Hisham <marwanhiisham@gmail.com>
2025-11-18 10:31:26 +0200

    (env): add typed envs

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,5 +1,19 @@
 import { CorsOptions } from 'cors';
+import { env } from './env';
 
 export const corsOptions: CorsOptions = {
-  origin(origin, callback) {},
+  origin(origin, callback) {
+    if (
+      env.NODE_ENV === 'development' ||
+      !origin ||
+      env.CORS_ORIGIN.includes(origin)
+    ) {
+      callback(null, true);
+    } else {
+      callback(
+        new Error(`CORS Error: ${origin} is not allowed by cors`),
+        false,
+      );
+    }
+  },
 };
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
+import { corsOptions } from './config/cors';
+import { env } from './config/env';
 
 const app = express();
 app.use(express.json());
@@ -9,9 +12,10 @@ app.use(cookieParser());
 app.use(helmet());
 app.use(compression({ threshold: 1024 }));
 app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
 
 (async () => {
-  app.listen(3000, () => {
-    console.log('Server started on http://localhost:3000');
+  app.listen(env.PORT, () => {
+    console.log(`Server started on http://localhost:${env.PORT}`);
   });
 })();
